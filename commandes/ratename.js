1ec549ca07956374f940e9cea753ffe3cc255c2c
ed0e&00aa0qd9f0z <46199479+r9q6j0@users.noreply.github.com>
2018-12-29 21:45:42 +0100

    Update ratename.js

--- commandes/ratename.js
+++ commandes/ratename.js
@@ -4,12 +4,14 @@ module.exports.run = async (bot, message, args) => {
  let randomNumber = Math.floor(Math.random() * 10 + 1)
   let wordRule = "un"
 
-  let admins = ["310045978923368448", "420902183350042627"]
-  if (message.author.id === admins) randomNumber = 10
+  const admin1 = ["420902183350042627"]
+  const admin2 = ["310045978923368448"]
+
+  if (message.author.id === admin1, admin2) randomNumber = 10
   if (message.mentions[0]) {
     message.channel.sendMessage(`Je note ${c.message.mentions[0].username} ${wordRule} **${randomNumber}**/10`)
   } else {
-    message.channel.sendMessage(`Je note ton nom, ${message.author.username} ${wordRule} **${randomNumber}**/10`)
+    message.channel.sendMessage(`Je note ton nom ${message.author.username} ${wordRule} **${randomNumber}**/10`)
   }
 }
 
