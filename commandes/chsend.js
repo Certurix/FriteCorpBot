ba7811135509f2e378eec1058f81c0de5322ec0a
ed0e&00aa0qd9f0z <46199479+r9q6j0@users.noreply.github.com>
2018-12-29 13:41:33 +0100

    Add files via upload

---
+++ commandes/chsend.js
@@ -0,0 +1,14 @@
+module.exports.run = async (bot, message) => {
+    const Discord = require('discord.js')
+    if (message.author.id !== "392441246238375936") return message.channel.send(":x:")
+    var args = message.content.split(" ").slice(0)
+    var Rargs = message.content.split(" ").slice(2).join(" ")
+    var channelID = args[1]
+    if (isNaN(args[1])) return message.reply("Ce n'est pas un ID.") //if args is Not A Number!
+    bot.channels.get(channelID).send(Rargs)
+    message.channel.send("Envoyé !").catch(console.error) //send the message
+}
+
+module.exports.help = {
+    name: "chsend"
+}
---
+++ commandes/eval.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig");
+module.exports.run = async (bot, msg) => {
+  const params = msg.content.split(" ");
+const args = msg.content.split(" ").slice(1);
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+  if(msg.author.id === "392441246238375936") {
+    try {
+         var code = args.join(" ");
+         var evaled = eval(code);
+
+         if (typeof evaled !== "string") {
+           evaled = require("util").inspect(evaled);
+         }
+         msg.channel.sendMessage(":floppy_disk:  Output:")
+         msg.channel.sendCode("js", clean(evaled, {depth: 20}));
+       } catch(err) {
+         msg.channel.sendMessage(`\`ERREUR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+       }
+     } else {
+    msg.channel.sendMessage("You do not have access to that command!");
+     }
+    }
+
+    module.exports.help = {
+        name: "eval"
+      }
